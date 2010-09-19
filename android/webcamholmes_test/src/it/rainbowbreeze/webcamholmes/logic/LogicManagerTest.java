d3ff2ff98d8f25d4e903eb1c45c43264e6cdb17e
alfredo.morresi <alfredo.morresi@bafd2136-80af-5e7d-71cc-0f315cf3a4f4>
2010-09-19 13:38:56 +0000

    added new working test class for the project

---
+++ android/webcamholmes_test/src/it/rainbowbreeze/webcamholmes/logic/LogicManagerTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2010 Alfredo Morresi
+ * 
+ * This file is part of WebcamHolmes project.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 
+ */
+
+package it.rainbowbreeze.webcamholmes.logic;
+
+import it.rainbowbreeze.libs.common.ServiceLocator;
+import it.rainbowbreeze.webcamholmes.data.AppPreferencesDao;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import static it.rainbowbreeze.libs.common.ContractHelper.*;
+
+/**
+ * @author Alfredo "Rainbowbreeze" Morresi
+ *
+ */
+public class LogicManagerTest extends AndroidTestCase {
+	//---------- Constructor
+
+	
+	
+	
+	//---------- Private fields
+	private LogicManager mLogicManager;
+
+
+	
+	
+	//---------- Test initialization
+
+
+	
+	
+	//---------- Test cases	
+	public void testExecuteEndTasks() throws IOException {
+		AppPreferencesDao appDao;
+		FileOutputStream fos;
+		String[] fileFullNames;
+		
+		appDao = checkNotNull(ServiceLocator.get(AppPreferencesDao.class), "AppPreferencesDao");
+		
+		fileFullNames = new String[]{
+				getContext().getFilesDir().getAbsolutePath() + File.separator + "Test1",
+				getContext().getFilesDir().getAbsolutePath() + File.separator + "Test2"
+		};
+		
+		//add test files
+		appDao.cleanResourcesToRemove();
+		for (String fileName:fileFullNames) {
+			//load some file that must be removed when the application ends
+			File file = new File(fileName);
+			fos = getContext().openFileOutput(file.getName(), Context.MODE_PRIVATE);
+			fos.write(("test file " + fileName).getBytes());
+			fos.close();
+			fos = null;
+			appDao.addResourceToRemove(fileName);
+		}
+		appDao.save();
+		
+		mLogicManager.executeEndTast(getContext());
+		
+		//checks if files were deleted
+		for (String fileName:fileFullNames) {
+			File file = new File(fileName);
+			assertFalse("File still exists: " + fileName, file.exists());
+		}
+	}
+
+	
+	
+	//---------- Private methods
+
+}
