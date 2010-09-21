a04cee0e3be849d12b91bc3714ef98aa1bdec986
alfredo.morresi <alfredo.morresi@bafd2136-80af-5e7d-71cc-0f315cf3a4f4>
2010-09-21 17:57:40 +0000

    added spider project

---
+++ android/trunk/webcamholmes-spider/src/it/rainbowbreeze/webcamholmes/spiders/BBCLondonTrafficSpider.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2010 Alfredo Morresi
+ * 
+ * This file is part of WebcamHolmes project.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.rainbowbreeze.webcamholmes.spiders;
+
+/**
+ * Create webcam from BBC London traffic webcams
+ * 
+ * @author Alfredo "Rainbowbreeze" Morresi
+ */
+public class BBCLondonTrafficSpider extends BaseSpider {
+
+	/* (non-Javadoc)
+	 * @see it.rainbowbreeze.webcamholmes.spiders.BaseSpider#parseResource()
+	 */
+	@Override
+	public String parseResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
